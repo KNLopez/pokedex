fac563eda345966f529e8551c0a7836da64b1945
Kurt Lopez <kurtdevph@gmail.com>
2019-04-26 01:32:10 +0800

    Updates

---
+++ precache-manifest.3c213bdc91c41bdbe345be5682484543.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6ec9cd2dc18368b3e3a6520fbd0eee",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "e0aa4b7b6416cda4a9ff",
+    "url": "/pokedex/static/css/main.f9717692.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/pokedex/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "e0aa4b7b6416cda4a9ff",
+    "url": "/pokedex/static/js/main.35e1915d.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
